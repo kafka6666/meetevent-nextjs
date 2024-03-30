38bfffc34b3e0fe06bc2f8b6fadd1abfc11ef180
Md. Abdullahel Kafi <140368515+kafka6666@users.noreply.github.com>
2024-03-30 17:37:22 +0600

    added header footer base components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">Meet Events</h1>
       <Button variant="destructive" className="px-10">
         Hello MeetEvent
       </Button>
